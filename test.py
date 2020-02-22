0f274d1a27247ed5e9b2616891ce69aa90c77054
OniKuma0217 <cheboyara0217@gmail.com>
2020-02-22 13:07:04 +0300

    Создали 2 файла

---
+++ test.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sin, cos, tan
+m = np.random.random([100,100])
+for i, v in np.ndenumerate(m):
+    x = i[0]
+    y = i[1]
+    m[x,y] += sin(x/10)*5
+plt.imshow(m)
+plt.show()
