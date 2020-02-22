0c9829d0f908d6ac214aa89cc71c90f2b1aee3ca
OniKuma0217 <cheboyara0217@gmail.com>
2020-02-22 13:12:00 +0300

    Добавили косинус

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ m = np.random.random([100,100])
 for i, v in np.ndenumerate(m):
     x = i[0]
     y = i[1]
-    m[x,y] += sin(x/10)*5
+    m[x,y] += sin(x/10)*5+cos(y/10)*5
 plt.imshow(m)
 plt.show()
